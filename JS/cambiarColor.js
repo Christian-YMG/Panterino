8cb7ddd0fb5b4bc60fabc360f93761c8ac32b473
Midoris-Alva <midorisvargas12@gmail.com>
2020-01-06 22:26:47 -0600

    Descripcion Avanzando

--- JS/cambiarColor.js
+++ JS/cambiarColor.js
@@ -15,24 +15,25 @@ let bandera = true;
 function cambiarColor(){
     
     if(bandera){
-        root.style.setProperty('--color-nav', 'rgb(59, 131, 189)');
-        nav.style.transition = 'all 1s';
-        root.style.setProperty('--color-letra', 'rgb(0, 0, 0)');
-        // items.transition = 'all 12s'; // variable de la clase
-        root.style.setProperty('--color-body', 'rgb(255, 255, 255)');
-        body.style.transition = 'all 1s'
-        img.src="IMG/Panterino.png";
-
-        bandera = false;
-    }
-    else if(bandera === false){
         root.style.setProperty('--color-nav', 'rgb(0, 0, 0)');
         nav.style.transition = 'all 1s';
         root.style.setProperty('--color-letra', 'rgb(255, 255, 255)');
         // items.transition = '12s';
         root.style.setProperty('--color-body', ' rgb(46, 46, 46)');
         body.style.transition = 'all 1s'
-        img.src="IMG/panterino-logo.png";
+        // img.src="IMG/panterino-logo.png";
+
+        bandera = false;
+    }
+    else if(bandera === false){
+       
+        root.style.setProperty('--color-nav', 'rgb(59, 131, 189)');
+        nav.style.transition = 'all 1s';
+        root.style.setProperty('--color-letra', 'rgb(0, 0, 0)');
+        // items.transition = 'all 12s'; // variable de la clase
+        root.style.setProperty('--color-body', 'rgb(255, 255, 255)');
+        body.style.transition = 'all 1s'
+        // img.src="IMG/Panterino.png"
         
         bandera = true;
     }

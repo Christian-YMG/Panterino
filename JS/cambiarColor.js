d667a8921473286dbe6074bdad263d7c6cf42da3
Christian-YMG <ymejiagalindo@hotmail.com>
2020-01-12 13:30:04 -0600

    Cambios Header, Tarjetas

--- JS/cambiarColor.js
+++ JS/cambiarColor.js
@@ -1,41 +1,41 @@
-let nav = document.getElementById("nav");
-let menuItems = document.getElementById("menu-items");
-let cambiar = document.getElementById("cambiar").addEventListener("click", cambiarColor);
-let img = document.getElementById("img");
-let body = document.getElementById("body");
-let imgNav = document.getElementById("imgNav")
+// let nav = document.getElementById("nav");
+// let menuItems = document.getElementById("menu-items");
+// let cambiar = document.getElementById("cambiar").addEventListener("click", cambiarColor);
+// let img = document.getElementById("img");
+// let body = document.getElementById("body");
+// let imgNav = document.getElementById("imgNav")
 
-let root = document.documentElement;
-// let body = document.getElementById("img-fondo");
-// let items = document.getElementsByClassName("items"); //investigar como cambiar valores a la clase
+// let root = document.documentElement;
+// // let body = document.getElementById("img-fondo");
+// // let items = document.getElementsByClassName("items"); //investigar como cambiar valores a la clase
 
 
-let bandera = true;
+// let bandera = true;
 
-function cambiarColor() {
+// function cambiarColor() {
 
-    if (bandera) {
-        root.style.setProperty('--color-nav', 'rgb(0, 0, 0)');
-        nav.style.transition = 'all 1s';
-        imgNav.src = "IMG/panterino-logo.png"
-        root.style.setProperty('--color-letra', 'rgb(255, 255, 255)');
-        // items.transition = '12s';
-        root.style.setProperty('--color-body', ' rgb(46, 46, 46)');
-        body.style.transition = 'all 1s'
-        // img.src="IMG/panterino-logo.png";
+//     if (bandera) {
+//         root.style.setProperty('--color-nav', 'rgb(0, 0, 0)');
+//         nav.style.transition = 'all 1s';
+//         imgNav.src = "IMG/panterino-logo.png"
+//         root.style.setProperty('--color-letra', 'rgb(255, 255, 255)');
+//         // items.transition = '12s';
+//         root.style.setProperty('--color-body', ' rgb(46, 46, 46)');
+//         body.style.transition = 'all 1s'
+//         // img.src="IMG/panterino-logo.png";
 
-        bandera = false;
-    } else if (bandera === false) {
+//         bandera = false;
+//     } else if (bandera === false) {
 
-        root.style.setProperty('--color-nav', 'rgb(59, 131, 189)');
-        nav.style.transition = 'all 1s';
-        imgNav.src = "IMG/Panterino.png"
-        root.style.setProperty('--color-letra', 'rgb(0, 0, 0)');
-        // items.transition = 'all 12s'; // variable de la clase
-        root.style.setProperty('--color-body', 'rgb(255, 255, 255)');
-        body.style.transition = 'all 1s'
-        // img.src="IMG/Panterino.png"
+//         root.style.setProperty('--color-nav', 'rgb(59, 131, 189)');
+//         nav.style.transition = 'all 1s';
+//         imgNav.src = "IMG/Panterino.png"
+//         root.style.setProperty('--color-letra', 'rgb(0, 0, 0)');
+//         // items.transition = 'all 12s'; // variable de la clase
+//         root.style.setProperty('--color-body', 'rgb(255, 255, 255)');
+//         body.style.transition = 'all 1s'
+//         // img.src="IMG/Panterino.png"
 
-        bandera = true;
-    }
-}
+//         bandera = true;
+//     }
+// }

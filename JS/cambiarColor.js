af0ad86dd6fdddb94a8a774335b1845e6969ca00
Christian-YMG <ymejiagalindo@hotmail.com>
2020-01-11 23:24:55 -0600

    Cartas Cambios[C

--- JS/cambiarColor.js
+++ JS/cambiarColor.js
@@ -1,9 +1,9 @@
-
 let nav = document.getElementById("nav");
 let menuItems = document.getElementById("menu-items");
-let cambiar = document.getElementById("cambiar").addEventListener("click" ,cambiarColor);
+let cambiar = document.getElementById("cambiar").addEventListener("click", cambiarColor);
 let img = document.getElementById("img");
 let body = document.getElementById("body");
+let imgNav = document.getElementById("imgNav")
 
 let root = document.documentElement;
 // let body = document.getElementById("img-fondo");
@@ -12,11 +12,12 @@ let root = document.documentElement;
 
 let bandera = true;
 
-function cambiarColor(){
-    
-    if(bandera){
+function cambiarColor() {
+
+    if (bandera) {
         root.style.setProperty('--color-nav', 'rgb(0, 0, 0)');
         nav.style.transition = 'all 1s';
+        imgNav.src = "IMG/panterino-logo.png"
         root.style.setProperty('--color-letra', 'rgb(255, 255, 255)');
         // items.transition = '12s';
         root.style.setProperty('--color-body', ' rgb(46, 46, 46)');
@@ -24,17 +25,17 @@ function cambiarColor(){
         // img.src="IMG/panterino-logo.png";
 
         bandera = false;
-    }
-    else if(bandera === false){
-       
+    } else if (bandera === false) {
+
         root.style.setProperty('--color-nav', 'rgb(59, 131, 189)');
         nav.style.transition = 'all 1s';
+        imgNav.src = "IMG/Panterino.png"
         root.style.setProperty('--color-letra', 'rgb(0, 0, 0)');
         // items.transition = 'all 12s'; // variable de la clase
         root.style.setProperty('--color-body', 'rgb(255, 255, 255)');
         body.style.transition = 'all 1s'
         // img.src="IMG/Panterino.png"
-        
+
         bandera = true;
     }
 }
